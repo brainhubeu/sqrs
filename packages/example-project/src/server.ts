2878428adea7340c407e5d57037f462d90fc3214
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-06-15 16:09:48 +0200

    fix lint

--- packages/example-project/src/server.ts
+++ packages/example-project/src/server.ts
@@ -1,12 +1,11 @@
 import { Container } from 'inversify';
 import express from 'express';
-import bodyParser from 'body-parser';
 
 import registerRoutes from './routes';
 
 export default function createServer (container: Container) {
   const app = express();
-  app.use(bodyParser.json());
+  app.use(express.json());
 
   registerRoutes(app, container);
 
